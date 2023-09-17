6417cd633fc3f7d5336bb5dd9a1de3bc09b40351
ANSHI14a <anshikasrivastava14903@gmail.com>
2023-09-17 21:24:15 +0530

    Add new file

--- small.java
+++ small.java
@@ -1,11 +1,24 @@
-public class small{
-    static void welcome(){
+import java.util.Scanner;
+class Algebra{
+     int add(int a,int b){
+        int ans=a+b;
+        return ans;
 
-    
-    System.out.println("Hello");
-    }
-    public static void main(String args[]){
-        welcome();
-    }
+     }
+     }
+     public class small{
+        public static void main(String[] args) {
+            Algebra obj=new Algebra();
+            Scanner sc=new Scanner (System.in);
+           
+            System.out.println("add two numbers");
+            int x =sc.nextInt();
+            int y =sc.nextInt();
+            
+            int ans=obj.add(x,y);
+            System.out.println(ans);
 
-}
+
+        }
+
+     }
